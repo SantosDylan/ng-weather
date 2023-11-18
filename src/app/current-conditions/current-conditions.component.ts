b0a0d9486f8b83d04d7d67958940cbb3dc9c05d3
SantosDylan <dylan.santos@epitech.eu>
2023-11-18 18:13:08 +0100

    fix: stop propagation when the suer try to close tab

--- src/app/current-conditions/current-conditions.component.ts
+++ src/app/current-conditions/current-conditions.component.ts
@@ -1,22 +1,27 @@
-import {Component, inject, Signal} from '@angular/core';
-import {WeatherService} from "../weather.service";
-import {LocationService} from "../location.service";
-import {Router} from "@angular/router";
-import {ConditionsAndZip} from '../conditions-and-zip.type';
+import { Component, inject, Signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConditionsAndZip } from '../conditions-and-zip.type';
+import { LocationService } from '../location.service';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-current-conditions',
   templateUrl: './current-conditions.component.html',
-  styleUrls: ['./current-conditions.component.css']
+  styleUrls: ['./current-conditions.component.css'],
 })
 export class CurrentConditionsComponent {
-
   private weatherService = inject(WeatherService);
   private router = inject(Router);
   protected locationService = inject(LocationService);
   protected currentConditionsByZip: Signal<ConditionsAndZip[]> = this.weatherService.getCurrentConditions();
 
-  showForecast(zipcode : string){
-    this.router.navigate(['/forecast', zipcode])
+  showForecast(event: Event, zipcode: string) {
+    event.stopPropagation();
+    this.router.navigate(['/forecast', zipcode]);
+  }
+
+  public onClose(event: Event, zip: string) {
+    event.stopPropagation();
+    this.locationService.removeLocation(zip);
   }
 }
