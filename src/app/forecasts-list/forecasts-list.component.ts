29dcb2bfaf4ef840b94aa4a94526b604cf8a7ef9
SantosDylan <dylan.santos@epitech.eu>
2023-11-18 18:14:01 +0100

    refactor: remove useless subscribe replaced by async pipe

--- src/app/forecasts-list/forecasts-list.component.ts
+++ src/app/forecasts-list/forecasts-list.component.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
-import {WeatherService} from '../weather.service';
-import {ActivatedRoute} from '@angular/router';
-import {Forecast} from './forecast.type';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { mergeMap, tap } from 'rxjs/operators';
+import { WeatherService } from '../weather.service';
+import { Forecast } from './forecast.type';
 
 @Component({
   selector: 'app-forecasts-list',
   templateUrl: './forecasts-list.component.html',
-  styleUrls: ['./forecasts-list.component.css']
+  styleUrls: ['./forecasts-list.component.css'],
 })
 export class ForecastsListComponent {
-
   zipcode: string;
-  forecast: Forecast;
+  data$: Observable<Forecast> | null = null;
 
-  constructor(protected weatherService: WeatherService, route : ActivatedRoute) {
-    route.params.subscribe(params => {
-      this.zipcode = params['zipcode'];
-      weatherService.getForecast(this.zipcode)
-        .subscribe(data => this.forecast = data);
-    });
+  constructor(protected weatherService: WeatherService, route: ActivatedRoute) {
+    this.data$ = route.params.pipe(
+      tap((params) => (this.zipcode = params['zipcode'])),
+      mergeMap((params) => this.weatherService.getForecast(params['zipcode']))
+    );
   }
 }
