24e48e6b6d96cbab8ad7de85cb66c2f108095e08
SantosDylan <dylan.santos@epitech.eu>
2023-11-22 18:44:15 +0100

    minor fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
-import { ModuleWithProviders }  from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {ForecastsListComponent} from "./forecasts-list/forecasts-list.component";
-import {MainPageComponent} from "./main-page/main-page.component";
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ForecastsListComponent } from "./forecasts-list/forecasts-list.component";
+import { MainPageComponent } from "./main-page/main-page.component";
 
 const appRoutes: Routes = [
   {
@@ -11,4 +11,4 @@ const appRoutes: Routes = [
     path: 'forecast/:zipcode', component: ForecastsListComponent
   }
 ];
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes, {});
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes, {useHash: true});
